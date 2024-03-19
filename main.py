8749716e52744ca5118d030ece8c95892129cbc3
Denys Marchenko <godden4ik@Denyss-MacBook-Pro.local>
2024-03-19 11:05:18 +0100

    task 2 done

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #Task 1
 
 squares = [x**2 for x in range(1, 11)]
+print("Task 1:", squares)
 
-print("List of squares of numbers from 1 to 10:")
-print(squares)
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end+1)]
+
+print("Task 2:", generate_squares(1, 10))

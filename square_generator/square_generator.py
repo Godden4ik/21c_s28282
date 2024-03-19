de2643e7eb3cdf1e5f2ee772b932c8af824d40df
Denys Marchenko <godden4ik@Denyss-MacBook-Pro.local>
2024-03-19 11:23:07 +0100

    task 6 done

---
+++ square_generator/square_generator.py
@@ -0,0 +1,12 @@
+import math
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("Task 6: End of range cannot be less than start.")
+        squares = [x**2 for x in range(start, end+1)]
+        return [math.sqrt(x) for x in squares]
+
+try:
+    print("Task 6:", SquareGenerator().generate_squares(1, 10))
+except ValueError as e:
+    print(e)
